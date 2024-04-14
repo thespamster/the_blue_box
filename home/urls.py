3b7918a10a47272289ebee4c995ca93ad88190af
GavB <gavin.brown@4uxdesign.com>
2024-04-14 20:33:43 +0100

    FEAT: add homepage to site. add custom CSS and Javascript. add sounds sub folder to static folder.

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'homepage'
 
 urlpatterns = [
     path('', views.index, name='home')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
