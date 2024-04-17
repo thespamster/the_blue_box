ad379cc3fba075497707e8e63cc0580649501a56
GavB <gavin.brown@4uxdesign.com>
2024-04-17 10:47:58 +0100

    BUILD: add 22 products to enable testing of site. add initial 6 categories. add product app to manage goods

--- blue_box_exchange/settings.py
+++ blue_box_exchange/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.apple',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
---
+++ products/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    display_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_display_name(self):
+        return self.display_name
+    
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    ean = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    tags = models.CharField(max_length=254)
+    
+    def __str__(self):
+        return self.name
