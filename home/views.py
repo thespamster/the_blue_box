3cc8e3d540a3159915b7a81f3e6b2a2b9aefde6f
GavB <gavin.brown@4uxdesign.com>
2024-04-23 12:40:54 +0100

    FEAT: add in the ability to quick search using tags

--- home/views.py
+++ home/views.py
@@ -11,23 +11,12 @@ def index(request):
 
     products = Product.objects.all()
     quantity = Product.objects.count()
-    start = random.randint(0, (quantity-6))
-    image1 = get_object_or_404(Product, pk=start).image_url
-    image2 = get_object_or_404(Product, pk=start+1).image_url
-    image3 = get_object_or_404(Product, pk=start+2).image_url
-    image4 = get_object_or_404(Product, pk=start+3).image_url
-    image5 = get_object_or_404(Product, pk=start+4).image_url
-    image6 = get_object_or_404(Product, pk=start+5).image_url
+    start = random.randint(0, (quantity-2))
+    featured_products = products[slice(start, start+2)]
 
     context = {
         'products': products,
-        'start_pos': start,
-        'image1': image1,
-        'image2': image2,
-        'image3': image3,
-        'image4': image4,
-        'image5': image5,
-        'image6': image6,
+        'featured_products': featured_products,
     }
 
     return render(request, 'home/index.html', context)
