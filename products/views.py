dd30aa8e89c208eeb216db2b143e8f66148b4413
GavB <gavin.brown@4uxdesign.com>
2024-04-27 15:54:47 +0100

    FEAT: add the option of a price search in the offcanvas menu

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,redirect, reverse, get_object_or_404
+from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
+from django.urls import reverse
 
 # Create your views here.
 
@@ -10,12 +11,21 @@ def all_products(request):
 
     products = Product.objects.all()
     categories = Category.objects.all()
-
-     
-    
     query = None
 
     if request.GET:
+
+        if 'price' in request.GET:
+            query = request.GET['price']
+            if not query:
+                messages.error(request, "No price search criteria!")
+                return redirect(reverse('products'))
+            
+            if query == '101':
+                query = 100
+                products = products.filter(price__gte=query).order_by('price')
+            else:
+                products = products.filter(price__lte=query).order_by('price')
 
         if 'category' in request.GET:
             query = request.GET['category']
@@ -25,7 +35,7 @@ def all_products(request):
             
             queries = categories.filter(name=query)
             queries = queries[slice(0,1)]
-            products = products.filter(category=queries)
+            products = products.filter(category=queries).order_by('price')
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -34,8 +44,7 @@ def all_products(request):
                 return redirect(reverse('products'))
 
             queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(tags__icontains=query)
-            products = products.filter(queries)
-
+            products = products.filter(queries).order_by('price')
 
     context = {
         'products': products,
