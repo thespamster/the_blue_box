06280fa0baf0a5c83073f7e8551d0bb7134618fd
GavB <gavin.brown@4uxdesign.com>
2024-05-06 18:28:40 +0100

    BUILD: make minor changes to custom allauth page styles

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 console.log("main.js loaded"); // REMEMBER TO REMOVE THIS LINE
 
-// When the user scrolls down 250px from the top of the document, show the button
+// When the user scrolls down 250px from the top of the document, show the 'back to the top' button
 $( window ).on( "scroll", function() {scrollFunction()});
 $("#topButton").click(function() {
     topFunction();
@@ -23,7 +23,19 @@ function topFunction() {
 
 // JS to style the allauth templates
 
-// Change style of account login page and Google login option
-$("#id_password_helptext a").text("Reset")
+$('input').css('border', 'none')
+$('input').css('width', '15rem') 
+$(":submit").css("padding", "5px") 
+$(":submit").addClass("btn-keep-shopping")
+
+// Change style of account login page
+$('#id_password_helptext a').text("Reset")
 $('label[for="id_login"]').css("display", "none")
 $('label[for="id_password"]').css("display", "none")
+
+// Change style of account signup page
+$('label[for="id_email"]').css("display", "none")
+$('label[for="id_email2"]').css("display", "none")
+$('label[for="id_username"]').css("display", "none")
+$('label[for="id_password1"]').css("display", "none")
+$('label[for="id_password2"]').css("display", "none")
