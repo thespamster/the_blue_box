b1adc749e251e75699b2acdc2659721e099bb304
GavB <gavin.brown@4uxdesign.com>
2024-05-08 19:18:01 +0100

    minor corrections to message boards text. tidied style.css

--- cart/views.py
+++ cart/views.py
@@ -15,35 +15,28 @@ def add_to_cart(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    ''' get the cart from the session or create one if it doesn't exist '''
     cart = request.session.get('cart', {})
-
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
-
     messages.success(request, f'Added  {product.name} to your cart')
-
     request.session['cart'] = cart
     return redirect(redirect_url)
 
 def update_cart(request, item_id):
     ''' Update the quantity or delete item from cart if quantity is 0 '''
-
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
-
     if quantity == 0:
         if item_id in list(cart.keys()):
             del request.session['cart'][item_id]
             messages.warning(request, f'Item {product.name} removed from your cart')
     else:
         cart[item_id] = quantity
-        messages.success(request, f'Updated quantity of {product.name} in your cart')
-        
-
-
+        messages.success(request, f'Updated the quantity of {product.name} to {quantity}')
     request.session['cart'] = cart
     return redirect(redirect_url)
