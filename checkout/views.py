8bce7056fa606bbb7f5be13abd4fd3a9542d17cc
GavB <gavin.brown@4uxdesign.com>
2024-05-12 14:53:49 +0100

    FEAT: add a Stripe card input box and make styling changes to the checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PFafQC0Oe68BG8jyigLuidI0hWmrsTpDzQsyPmUnsqOEYTJ5INJryAgHQlXL6r9C6xE2DvCeswoAbi9jlkqREqB0013ST5RBL',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
