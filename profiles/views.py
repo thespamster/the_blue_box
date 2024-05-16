7b42d6800402660fee8222b1bbc391cd301150d0
GavB <gavin.brown@4uxdesign.com>
2024-05-16 16:12:43 +0100

    FEAT: refined default delivery section

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from .forms import UserProfileForm
 
 # Create your views here.
 def profile(request):
     ''' Display the user's profile '''
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    form = UserProfileForm(instance=profile)
+    order = profile.orders.all()
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'order': order,
     }
 
     return render(request, template, context)
