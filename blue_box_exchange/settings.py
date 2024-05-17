c2eeb1b2f3700047023dc03b5693e7802f3cfe2d
GavB <gavin.brown@4uxdesign.com>
2024-05-17 18:24:58 +0100

    FEAT: complete the basic account setup

--- blue_box_exchange/settings.py
+++ blue_box_exchange/settings.py
@@ -209,8 +209,7 @@ MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
 FREE_DELIVERY = 50
 STANDARD_DELIVERY_COST = 2.99
 STRIPE_CURRENCY = 'gbp'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+DEFAULT_FROM_EMAIL = 'orders@blueboxshop.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
